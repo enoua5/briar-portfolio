4b273b7a9a64042f644c51a0197666b18002968a
Iota <enoua5@gmail.com>
2024-05-06 20:38:21 -0600

    Fix image urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.main}>
-      <img src="/planets.png"/>
-      <img src="/hollow_knight.png"/>
-      <img src="/majora.png"/>
+      <img src="planets.png"/>
+      <img src="hollow_knight.png"/>
+      <img src="majora.png"/>
       <img src="hells_company.png"/>
       <img src="zeldas.png"/>
     </div>
